6adf4a7597c2ddb125149a97760a53a22161e9d0
Shrikant Patnaik <shrikant.patnaik@gmail.com>
2018-10-05 14:18:57 -0400

    Add edit, update show and index listing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  config.web_console.whitelisted_ips = %w(210.6.25.142 10.1.0.6)
 end
